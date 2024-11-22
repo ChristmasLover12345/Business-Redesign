fa5f45bd27aa66d7ae2c8e2f8354f00ea770f78d
MandoxaElemental <gprcards@gmail.com>
2024-11-22 15:09:57 -0800

    fixed the js

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,9 @@ let card3 = document.getElementById('card3');
 
 
 addEventListener("scroll", (event) => {
-    card1.className = 'card' + ' fadeIn';
-    card2.className = 'card' + ' fadeIn';
-    card3.className = 'card' + ' fadeIn';
+    card1.className = 'card-img' + ' fadeIn';
+    card2.className = 'card-img' + ' fadeIn';
+    card3.className = 'card-img' + ' fadeIn';
 });
 
 onscroll = (event) => {};
